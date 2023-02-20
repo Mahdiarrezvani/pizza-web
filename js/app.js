47713bb6b3782c52c6facd310a39b15b17d0f88e
mahdiar <mahdiarrezvani59@gmail.com>
2023-02-19 17:02:14 -0800

    1

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 let imagesGallerySrc = ['asset/Gallery/Rectangle 11.png', 'asset/Gallery/Rectangle 12.png', 'asset/Gallery/Rectangle 13.png', 'asset/Gallery/Rectangle 14.png'];
+let starSolid = '<i class="fa-sharp fa-solid fa-star"></i><i class="fa-sharp fa-solid fa-star"></i><i class="fa-sharp fa-solid fa-star"></i><i class="fa-sharp fa-solid fa-star"></i>';
+let starRegular = '<i class="fa-sharp fa-regular fa-star"></i><i class="fa-sharp fa-regular fa-star"></i><i class="fa-sharp fa-regular fa-star"></i>';
 let infoStaff = [
-    { name: 'braylen earleen', star: '* * * * * *', type: 'chef', srcImage: 'asset/person/Ellipse 12-1.png' },
-    { name: 'braylen earleen', star: '* * * * * *', type: 'chef', srcImage: 'asset/person/Ellipse 12-2.png' },
-    { name: 'braylen earleen', star: '* * * * * *', type: 'chef', srcImage: 'asset/person/Ellipse 12.png' },
-    // { name: 'braylen earleen', star: '* * * * * *', type: 'chef', srcImage: '' },
-    // { name: 'braylen earleen', star: '* * * * * *', type: 'chef', srcImage: '' },
+    { name: 'braylen earleen', star: starSolid + starRegular, type: 'chef', srcImage: 'asset/person/Ellipse 12-1.png' },
+    { name: 'braylen earleen', star: starSolid + starRegular, type: 'chef', srcImage: 'asset/person/Ellipse 12-2.png' },
+    { name: 'braylen earleen', star: starSolid + starRegular, type: 'chef', srcImage: 'asset/person/Ellipse 12.png' },
+    // { name: 'braylen earleen', star: starSolid + starRegular, type: 'chef', srcImage: '' },
 ];
 // 
 let galleryContainer = document.querySelector('.image-gallery');
